abc3b85d41eaacb62e07e4bd6985be68d35b3d4f
Jose Sauco <jose.saucod@gmail.com>
2017-01-19 23:13:17 +0100

    Revision 3 de scripts de importacion

--- test/import_csv.R
+++ test/import_csv.R
@@ -1,116 +1,117 @@
-read_epa <- function(x)
-{
-  # ----CONTROL
-  CICLO <- as.numeric(substr(fichero, 1, 3))
-  # ----IDEN
-  CCAA <- substr(fichero, 4, 5)
-  PROV <- substr(fichero, 6, 7)
-  NVIVI <- substr(fichero, 8, 12)
-  NIVEL <- substr(fichero, 13, 13)
-  NPERS <- substr(fichero, 14, 15)
-  # ----A
-  EDAD5 <- substr(fichero, 16, 17)
-  RELPP1 <- substr(fichero, 18, 18)
-  SEXO1 <- factor(substr(fichero, 19, 19),c(1,6),c("Hombre", "Mujer"))
-  NCONY <- substr(fichero, 20, 21)
-  NPADRE <- substr(fichero, 22, 23)
-  NMADRE <- substr(fichero, 24, 25)
-  RELLMILI <- substr(fichero, 26, 26)
-  ECIV1 <- substr(fichero, 27, 27)
-  # ----NACIM
-  PRONA1 <- substr(fichero, 28, 29)
-  REGNA1 <- substr(fichero, 30, 32)
-  NAC1 <- substr(fichero, 33, 33)
-  EXREGNA1 <- substr(fichero, 34, 36)
-  ANORE1 <- substr(fichero, 37, 38)
-  # ----B
-  NFORMA <- substr(fichero, 39, 40)
-  RELLB <- substr(fichero, 41, 42)
-  EDADEST <- substr(fichero, 43, 45)
-  CURSR <- substr(fichero, 46, 46)
-  NCURSR <- substr(fichero, 47, 48)
-  CURSNR <- substr(fichero, 49, 49)
-  NCURNR <- substr(fichero, 50, 51)
-  HCURNR <- substr(fichero, 52, 54)
-  RELLB <- substr(fichero, 55, 56)
-  # ----C
-  TRAREM <- substr(fichero, 57, 57)
-  AYUDFA <- substr(fichero, 58, 58)
-  AUSENT <- substr(fichero, 59, 59)
-  RZNOTB <- substr(fichero, 60, 61)
-  VINCUL <- substr(fichero, 62, 63)
-  NUEVEM <- substr(fichero, 64, 64)
-  # ----D
-  OCUP1 <- substr(fichero, 65, 65)
-  ACT1 <- substr(fichero, 66, 66)
-  SITU <- substr(fichero, 67, 68)
-  SP <- substr(fichero, 69, 69)
-  DUCON1 <- substr(fichero, 70, 70)
-  DUCON2 <- substr(fichero, 71, 71)
-  DUCON3 <- substr(fichero, 72, 73)
-  # ----TCONT
-  TCONTM <- substr(fichero, 74, 75)
-  TCONTD <- substr(fichero, 76, 77)
-  DREN <- substr(fichero, 78, 80)
-  DCOM <- substr(fichero, 81, 83)
-  # ----UBICA
-  PROEST <- factor(gsub(" ","",substr(fichero, 84, 85)),exclude="")
-  REGEST <- substr(fichero, 86, 88)
-  PARCO1 <- substr(fichero, 89, 89)
-  PARCO2 <- substr(fichero, 90, 91)
-  HORASP <- substr(fichero, 92, 95)
-  HORASH <- substr(fichero, 96, 99)
-  HORASE <- substr(fichero, 100, 103)
-  EXTRA <- substr(fichero, 104, 104)
-  EXTPAG <- substr(fichero, 105, 108)
-  EXTNPG <- substr(fichero, 109, 112)
-  RZDIFH <- substr(fichero, 113, 114)
-  TRAPLU <- substr(fichero, 115, 115)
-  OCUPLU1 <- substr(fichero, 116, 116)
-  ACTPLU1 <- substr(fichero, 117, 117)
-  SITPLU <- substr(fichero, 118, 119)
-  HORPLU <- substr(fichero, 120, 123)
-  MASHOR <- substr(fichero, 124, 124)
-  DISMAS <- substr(fichero, 125, 125)
-  RZNDISH <- substr(fichero, 126, 127)
-  HORDES <- substr(fichero, 128, 129)
-  BUSOTR <- substr(fichero, 130, 130)
-  # ----E
-  BUSCA <- substr(fichero, 131, 131)
-  DESEA <- substr(fichero, 132, 132)
-  FOBACT <- substr(fichero, 133, 133)
-  NBUSCA <- substr(fichero, 134, 135)
-  ASALA <- substr(fichero, 136, 136)
-  EMBUS <- substr(fichero, 137, 137)
-  ITBU <- substr(fichero, 138, 139)
-  DISP <- substr(fichero, 140, 140)
-  RZNDIS <- substr(fichero, 141, 141)
-  # ----F
-  EMPANT <- substr(fichero, 142, 142)
-  DTANT <- substr(fichero, 143, 145)
-  OCUPA <- substr(fichero, 146, 146)
-  ACTA <- substr(fichero, 147, 147)
-  SITUA <- substr(fichero, 148, 149)
-  # ----G
-  OFEMP <- substr(fichero, 150, 150)
-  # ----H
-  SIDI1 <- substr(fichero, 151, 152)
-  SIDI2 <- substr(fichero, 153, 154)
-  SIDI3 <- substr(fichero, 155, 156)
-  SIDAC1 <- substr(fichero, 157, 157)
-  SIDAC2 <- substr(fichero, 158, 158)
-  # ----I
-  MUN1 <- substr(fichero, 159, 159)
-  PRORE1 <- substr(fichero, 160, 161)
-  REPAIRE1 <- substr(fichero, 162, 164)
-  # ----J
-  TRAANT <- substr(fichero, 165, 165)
-  # ----VARDER1
-  AOI <- substr(fichero, 166, 167)
-  CSE <- substr(fichero, 168, 169)
-  # ----VARDERC
-  FACTOREL <- substr(fichero, 170, 176)
+fichero<-readLines("datos_old_full")
 
+
+  # ----CONTROL
+  CICLO<-factor(gsub(" ","",substr(fichero, 1,3)),exclude="")
+  # ----IDEN
+  CCAA<-factor(gsub(" ","",substr(fichero, 4,5)),exclude="")
+  PROV<-factor(gsub(" ","",substr(fichero, 6,7)),exclude="")
+  NVIVI<-factor(gsub(" ","",substr(fichero, 8,12)),exclude="")
+  NIVEL<-factor(gsub(" ","",substr(fichero, 13,13)),exclude="")
+  NPERS<-factor(gsub(" ","",substr(fichero, 14,15)),exclude="")
+  # ----A
+  EDAD5<-factor(gsub(" ","",substr(fichero, 16,17)),exclude="")
+  RELPP1<-factor(gsub(" ","",substr(fichero, 18,18)),exclude="")
+  SEXO1<-factor(gsub(" ","",substr(fichero, 19,19)),exclude="")
+  NCONY<-factor(gsub(" ","",substr(fichero, 20,21)),exclude="")
+  NPADRE<-factor(gsub(" ","",substr(fichero, 22,23)),exclude="")
+  NMADRE<-factor(gsub(" ","",substr(fichero, 24,25)),exclude="")
+  RELLMILI<-factor(gsub(" ","",substr(fichero, 26,26)),exclude="")
+  ECIV1<-factor(gsub(" ","",substr(fichero, 27,27)),exclude="")
+  # ----NACIM
+  PRONA1<-factor(gsub(" ","",substr(fichero, 28,29)),exclude="")
+  REGNA1<-factor(gsub(" ","",substr(fichero, 30,32)),exclude="")
+  NAC1<-factor(gsub(" ","",substr(fichero, 33,33)),exclude="")
+  EXREGNA1<-factor(gsub(" ","",substr(fichero, 34,36)),exclude="")
+  ANORE1<-factor(gsub(" ","",substr(fichero, 37,38)),exclude="")
+  # ----B
+  NFORMA<-factor(gsub(" ","",substr(fichero, 39,40)),exclude="")
+  RELLB<-factor(gsub(" ","",substr(fichero, 41,42)),exclude="")
+  EDADEST<-factor(gsub(" ","",substr(fichero, 43,45)),exclude="")
+  CURSR<-factor(gsub(" ","",substr(fichero, 46,46)),exclude="")
+  NCURSR<-factor(gsub(" ","",substr(fichero, 47,48)),exclude="")
+  CURSNR<-factor(gsub(" ","",substr(fichero, 49,49)),exclude="")
+  NCURNR<-factor(gsub(" ","",substr(fichero, 50,51)),exclude="")
+  HCURNR<-factor(gsub(" ","",substr(fichero, 52,54)),exclude="")
+  RELLB<-factor(gsub(" ","",substr(fichero, 55,56)),exclude="")
+  # ----C
+  TRAREM<-factor(gsub(" ","",substr(fichero, 57,57)),exclude="")
+  AYUDFA<-factor(gsub(" ","",substr(fichero, 58,58)),exclude="")
+  AUSENT<-factor(gsub(" ","",substr(fichero, 59,59)),exclude="")
+  RZNOTB<-factor(gsub(" ","",substr(fichero, 60,61)),exclude="")
+  VINCUL<-factor(gsub(" ","",substr(fichero, 62,63)),exclude="")
+  NUEVEM<-factor(gsub(" ","",substr(fichero, 64,64)),exclude="")
+  # ----D
+  OCUP1<-factor(gsub(" ","",substr(fichero, 65,65)),exclude="")
+  ACT11<-factor(gsub(" ","",substr(fichero, 66,66)),exclude="")
+  SITU<-factor(gsub(" ","",substr(fichero, 67,68)),exclude="")
+  SP<-factor(gsub(" ","",substr(fichero, 69,69)),exclude="")
+  DUCON1<-factor(gsub(" ","",substr(fichero, 70,70)),exclude="")
+  DUCON2<-factor(gsub(" ","",substr(fichero, 71,71)),exclude="")
+  DUCON3<-factor(gsub(" ","",substr(fichero, 72,73)),exclude="")
+  # ----TCONT
+  TCONTM<-factor(gsub(" ","",substr(fichero, 74,75)),exclude="")
+  TCONTD<-factor(gsub(" ","",substr(fichero, 76,77)),exclude="")
+  DREN<-factor(gsub(" ","",substr(fichero, 78,80)),exclude="")
+  DCOM<-factor(gsub(" ","",substr(fichero, 81,83)),exclude="")
+  # ----UBICA
+  PROEST<-factor(gsub(" ","",substr(fichero, 84,85)),exclude="")
+  REGEST<-factor(gsub(" ","",substr(fichero, 86,88)),exclude="")
+  PARCO1<-factor(gsub(" ","",substr(fichero, 89,89)),exclude="")
+  PARCO2<-factor(gsub(" ","",substr(fichero, 90,91)),exclude="")
+  HORASP<-factor(gsub(" ","",substr(fichero, 92,95)),exclude="")
+  HORASH<-factor(gsub(" ","",substr(fichero, 96,99)),exclude="")
+  HORASE<-factor(gsub(" ","",substr(fichero, 100,103)),exclude="")
+  EXTRA<-factor(gsub(" ","",substr(fichero, 104,104)),exclude="")
+  EXTPAG<-factor(gsub(" ","",substr(fichero, 105,108)),exclude="")
+  EXTNPG<-factor(gsub(" ","",substr(fichero, 109,112)),exclude="")
+  RZDIFH<-factor(gsub(" ","",substr(fichero, 113,114)),exclude="")
+  TRAPLU<-factor(gsub(" ","",substr(fichero, 115,115)),exclude="")
+  OCUPLU1<-factor(gsub(" ","",substr(fichero, 116,116)),exclude="")
+  ACTPLU1<-factor(gsub(" ","",substr(fichero, 117,117)),exclude="")
+  SITPLU<-factor(gsub(" ","",substr(fichero, 118,119)),exclude="")
+  HORPLU<-factor(gsub(" ","",substr(fichero, 120,123)),exclude="")
+  MASHOR<-factor(gsub(" ","",substr(fichero, 124,124)),exclude="")
+  DISMAS<-factor(gsub(" ","",substr(fichero, 125,125)),exclude="")
+  RZNDISH<-factor(gsub(" ","",substr(fichero, 126,127)),exclude="")
+  HORDES<-factor(gsub(" ","",substr(fichero, 128,129)),exclude="")
+  BUSOTR<-factor(gsub(" ","",substr(fichero, 130,130)),exclude="")
+  # ----E
+  BUSCA<-factor(gsub(" ","",substr(fichero, 131,131)),exclude="")
+  DESEA<-factor(gsub(" ","",substr(fichero, 132,132)),exclude="")
+  FOBACT<-factor(gsub(" ","",substr(fichero, 133,133)),exclude="")
+  NBUSCA<-factor(gsub(" ","",substr(fichero, 134,135)),exclude="")
+  ASALA<-factor(gsub(" ","",substr(fichero, 136,136)),exclude="")
+  EMBUS<-factor(gsub(" ","",substr(fichero, 137,137)),exclude="")
+  ITBU<-factor(gsub(" ","",substr(fichero, 138,139)),exclude="")
+  DISP<-factor(gsub(" ","",substr(fichero, 140,140)),exclude="")
+  RZNDIS<-factor(gsub(" ","",substr(fichero, 141,141)),exclude="")
+  # ----F
+  EMPANT<-factor(gsub(" ","",substr(fichero, 142,142)),exclude="")
+  DTANT<-factor(gsub(" ","",substr(fichero, 143,145)),exclude="")
+  OCUPA<-factor(gsub(" ","",substr(fichero, 146,146)),exclude="")
+  ACTA<-factor(gsub(" ","",substr(fichero, 147,147)),exclude="")
+  SITUA<-factor(gsub(" ","",substr(fichero, 148,149)),exclude="")
+  # ----G
+  OFEMP<-factor(gsub(" ","",substr(fichero, 150,150)),exclude="")
+  # ----H
+  SIDI1<-factor(gsub(" ","",substr(fichero, 151,152)),exclude="")
+  SIDI2<-factor(gsub(" ","",substr(fichero, 153,154)),exclude="")
+  SIDI3<-factor(gsub(" ","",substr(fichero, 155,156)),exclude="")
+  SIDAC1<-factor(gsub(" ","",substr(fichero, 157,157)),exclude="")
+  SIDAC2<-factor(gsub(" ","",substr(fichero, 158,158)),exclude="")
+  # ----I
+  MUN1<-factor(gsub(" ","",substr(fichero, 159,159)),exclude="")
+  PRORE1<-factor(gsub(" ","",substr(fichero, 160,161)),exclude="")
+  REPAIRE1<-factor(gsub(" ","",substr(fichero, 162,164)),exclude="")
+  # ----J
+  TRAANT<-factor(gsub(" ","",substr(fichero, 165,165)),exclude="")
+  # ----VARDER1
+  AOI<-factor(gsub(" ","",substr(fichero, 166,167)),exclude="")
+  CSE<-factor(gsub(" ","",substr(fichero, 168,169)),exclude="")
+  # ----VARDERC
+  FACTOREL<-factor(gsub(" ","",substr(fichero, 170,176)),exclude="")
+  
   result_frame <-
     data.frame(
       CICLO,
@@ -148,7 +149,7 @@ read_epa <- function(x)
       VINCUL,
       NUEVEM,
       OCUP1,
-      ACT1,
+      ACT11,
       SITU,
       SP,
       DUCON1,
@@ -207,5 +208,7 @@ read_epa <- function(x)
       CSE,
       FACTOREL
     )
-  return(result_frame)
-}
+
+  saveRDS(result_frame,file="dframe_old.Rda")
+  setwd("D:/workarea")
+  dframe<-readRDS(file="dframe_new.Rda")
--- test/import_to_db.R
+++ test/import_to_db.R
@@ -16,9 +16,9 @@ time_to_hours <- function(x) {
 }
 
 column_widths<-c(3, 2, 2, 5, 1, 2, 2, 1, 1, 2, 2, 2, 1, 1, 2, 3, 1, 3, 2, 2, 2, 3, 1, 2, 1, 2, 3, 2, 1, 1, 1, 2, 2, 1, 1, 1, 2, 1, 1, 1, 2, 2, 2, 3, 3, 2, 3, 1, 2, 4, 4, 4, 1, 4, 4, 2, 1, 1, 1, 2, 4, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 3, 1, 1, 2, 1, 2, 2, 2, 1, 1, 1, 2, 3, 1, 2, 2, 7)
-column_names<-c("CICLO", "CCAA", "PROV", "NVIVI", "NIVEL", "NPERS", "EDAD5", "RELPP1", "SEXO1", "NCONY", "NPADRE", "NMADRE", "RELLMILI", "ECIV1", "PRONA1", "REGNA1", "NAC1", "EXREGNA1", "ANORE1", "NFORMA", "RELLB", "EDADEST", "CURSR", "NCURSR", "CURSNR", "NCURNR", "HCURNR", "RELLB1", "TRAREM", "AYUDFA", "AUSENT", "RZNOTB", "VINCUL", "NUEVEM", "OCUP1", "ACT1", "SITU", "SP", "DUCON1", "DUCON2", "DUCON3", "TCONTM", "TCONTD", "DREN", "DCOM", "PROEST", "REGEST", "PARCO1", "PARCO2", "HORASP", "HORASH", "HORASE", "EXTRA", "EXTPAG", "EXTNPG", "RZDIFH", "TRAPLU", "OCUPLU1", "ACTPLU1", "SITPLU", "HORPLU", "MASHOR", "DISMAS", "RZNDISH", "HORDES", "BUSOTR", "BUSCA", "DESEA", "FOBACT", "NBUSCA", "ASALA", "EMBUS", "ITBU", "DISP", "RZNDIS", "EMPANT", "DTANT", "OCUPA", "ACTA", "SITUA", "OFEMP", "SIDI1", "SIDI2", "SIDI3", "SIDAC1", "SIDAC2", "MUN1", "PRORE1", "REPAIRE1", "TRAANT", "AOI", "CSE", "FACTOREL")
-col_types<-("nccnc-n-c----cccc--c-nc-c---ccc--cccc-cc-nnnnccc-nnnc---c----c---ccc-c---cccn---ccccccc---ccn")
-dframe<-read_fwf("data/datos_old_full", fwf_widths(column_widths, column_names), col_types)
+column_names<-c("CICLO", "CCAA", "PROV", "NVIVI", "NIVEL", "NPERS", "EDAD5", "RELPP", "SEXO", "NCONY", "NPADRE", "NMADRE", "RELLMILI", "ECIV", "PRONA", "REGNA", "NAC", "EXREGNA", "ANORE", "NFORMA", "RELLB", "EDADEST", "CURSR", "NCURSR", "CURSNR", "NCURNR", "HCURNR", "RELLB1", "TRAREM", "AYUDFA", "AUSENT", "RZNOTB", "VINCUL", "NUEVEM", "OCUP", "ACT", "SITU", "SP", "DUCON1", "DUCON2", "DUCON3", "TCONTM", "TCONTD", "DREN", "DCOM", "PROEST", "REGEST", "PARCO1", "PARCO2", "HORASP", "HORASH", "HORASE", "EXTRA", "EXTPAG", "EXTNPG", "RZDIFH", "TRAPLU", "OCUPLU", "ACTPLU", "SITPLU", "HORPLU", "MASHOR", "DISMAS", "RZNDISH", "HORDES", "BUSOTR", "BUSCA", "DESEA", "FOBACT", "NBUSCA", "ASALA", "EMBUS", "ITBU", "DISP", "RZNDIS", "EMPANT", "DTANT", "OCUPA", "ACTA", "SITUA", "OFEMP", "SIDI1", "SIDI2", "SIDI3", "SIDAC1", "SIDAC2", "MUN", "PRORE", "REPAIRE", "TRAANT", "AOI", "CSE", "FACTOREL")
+col_types<-("nccnc-n-c----cccccnc-nccccn-cccccccccccccnnnnccccnnncnncccccncccncccccccccccncccccccc-ccc-ccn")
+dframe<-read_fwf("data/raw_new/datos_2016t3", fwf_widths(column_widths, column_names), col_types)
 
 # ------- RETROCOMPATIBILIDAD
 # REVISAR EL NFORMA
@@ -30,19 +30,30 @@ dframe$NUEVEM[dframe$NUEVEM=="6" & !is.na(dframe$NUEVEM)]<-"3" # Retrocompatibil
 
 dframe$NFORMA[dframe$CICLO<130]<-mapvalues(dframe$NFORMA[dframe$CICLO<130],from=c("01","02","03","04","05","06","07","08","09","10","11","12","13","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"), to=c("AN","AN","P2","S1","S1","S1","S1","SG","SP","SP","SP","SP","SP","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU")) #Retrocompatibilidad
 
+dframe$NCURSR[dframe$CICLO<130]<-mapvalues(dframe$NCURSR[dframe$CICLO<130],from=c("03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40"), to=c("PR","S1","S1","S1","S1","SG","SP","SP","SP","SP","SP","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","SU","ED","ED","ED","ED","EM","PE","PE","PE","PE")) #Retrocompatibilidad
+
+
 dframe$SITU[dframe$CICLO<130]<-mapvalues(dframe$SITU[dframe$CICLO<130],from=c("1","2","3","4","5","6","7","8","9"), to=c("01","01","03","03","05","06","07","08","09")) #Retrocompatibilidad
+dframe$SITPLU[dframe$CICLO<130]<-mapvalues(dframe$SITPLU[dframe$CICLO<130],from=c("1","2","3","4","5","6","7","8","9"), to=c("01","01","03","03","05","06","07","08","09")) #Retrocompatibilidad
 dframe$CURSNR[dframe$CICLO<130]<-mapvalues(dframe$NCURSR[dframe$CICLO<130],from=c("PR","S1","SG","SP","SU","ED","EM","PE"), to=c("3","3","3","3","3","1","1","1"))
+dframe$NCURNR[(dframe$NCURSR=="ED"|dframe$NCURSR=="EM"|dframe$NCURSR=="PE") & !is.na(dframe$NCURSR)]<-dframe$NCURSR[(dframe$NCURSR=="ED"|dframe$NCURSR=="EM"|dframe$NCURSR=="PE") & !is.na(dframe$NCURSR)] # Retro compatibilidad
 dframe$DUCON1[dframe$CICLO<130]<-mapvalues(dframe$DUCON2[dframe$CICLO<130],from=c("1","2","3","4","5","6","7","8","9","0"), to=c("1","1","6","6","6","6","6","6","6","6")) #Retrocompatibilidad
+dframe$DUCON3[dframe$CICLO<130]<-mapvalues(dframe$DUCON2[dframe$CICLO<130],from=c("1","2","3","4","5","6","7","8","9","0"), to=c(NA,NA,"02","03","04","05","06","07","08","00")) #Retrocompatibilidad
 dframe$DUCON2[dframe$CICLO<130]<-mapvalues(dframe$DUCON2[dframe$CICLO<130],from=c("1","2","3","4","5","6","7","8","9","0"), to=c("1","6",NA,NA,NA,NA,NA,NA,NA,NA)) #Retrocompatibilidad
+dframe$PARCO2[dframe$CICLO<130]<-mapvalues(dframe$PARCO1[dframe$CICLO<130],from=c("1","2","3","4","5","6","7","8","9"), to=c(NA,"01","02","03","04","05","06","07","00")) #Retrocompatibilidad
 dframe$PARCO1[dframe$CICLO<130]<-mapvalues(dframe$PARCO1[dframe$CICLO<130],from=c("1","2","3","4","5","6","7","8","9"), to=c("1","6","6","6","6","6","6","6","6")) #Retrocompatibilidad
 dframe$HORASH[dframe$CICLO<130]<-dframe$HORASH[dframe$CICLO<130]*100 # Retrocompatiblidad
 dframe$HORASE[dframe$CICLO<130]<-dframe$HORASE[dframe$CICLO<130]*100 # Retrocompatiblidad
+dframe$HORPLU[dframe$CICLO<130]<-dframe$HORPLU[dframe$CICLO<130]*100 # Retrocompatiblidad
 dframe$MASHOR[dframe$CICLO<130]<-mapvalues(dframe$MASHOR[dframe$CICLO<130],from=c("1","2","3","4","5","6"), to=c("1","1","1","1","2","3")) #Retrocompatibilidad
+dframe$RZNDISH[dframe$CICLO<130]<-mapvalues(dframe$DISMAS[dframe$CICLO<130],from=c("1","2","3","4","5","6","7"), to=c(NA,"01","05","02","03","04","04")) #Retrocompatibilidad
+dframe$DISMAS[dframe$CICLO<130]<-mapvalues(dframe$DISMAS[dframe$CICLO<130],from=c("1","2","3","4","5","6","7"), to=c("1","6","6","6","6","6","6")) #Retrocompatibilidad
 dframe$DESEA[dframe$CICLO<130]<-mapvalues(dframe$BUSCA[dframe$CICLO<130],from=c("1","2","3"), to=c(NA,"1","6")) #Retrocompatibilidad
 dframe$BUSCA[dframe$CICLO<130]<-mapvalues(dframe$BUSCA[dframe$CICLO<130],from=c("1","2","3"), to=c("1","6","6")) #Retrocompatibilidad
 dframe$NBUSCA[dframe$CICLO<130]<-mapvalues(dframe$NBUSCA[dframe$CICLO<130],from=c("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15"), to=c("01","08","08","08","02","08","08","08","08","03","05","06","07","08","08")) #Retrocompatibilidad
 dframe$RZNDIS[dframe$CICLO<130]<-mapvalues(dframe$DISP[dframe$CICLO<130],from=c("1","2","3","4","5","6","7"), to=c(NA,"1","5","2","3","4","4")) #Retrocompatibilidad
 dframe$DISP[dframe$CICLO<130]<-mapvalues(dframe$DISP[dframe$CICLO<130],from=c("1","2","3","4","5","6","7"), to=c("1","6","6","6","6","6","6")) #Retrocompatibilidad
+dframe$SITUA[dframe$CICLO<130]<-mapvalues(dframe$SITUA[dframe$CICLO<130],from=c("01","02","03","04","05","06","07","08","09"), to=c("01","01","03","03","05","06","07","08","09")) #Retrocompatibilidad
 dframe$SIDI1[dframe$CICLO<130]<-mapvalues(dframe$SIDI1[dframe$CICLO<130],from=c("1","2","3","4","5","6","7"), to=c("01","02","03","04","05","06","07")) #Retrocompatibilidad
 dframe$SIDI2[dframe$CICLO<130]<-mapvalues(dframe$SIDI2[dframe$CICLO<130],from=c("1","2","3","4","5","6","7"), to=c("01","02","03","04","05","06","07")) #Retrocompatibilidad
 dframe$SIDI3[dframe$CICLO<130]<-mapvalues(dframe$SIDI3[dframe$CICLO<130],from=c("1","2","3","4","5","6","7"), to=c("01","02","03","04","05","06","07")) #Retrocompatibilidad
@@ -64,12 +75,12 @@ lista_region<-list("Resto de Europa" = "100", "UE-15" = "115", "UE-25" = "125", 
 
 levels(dframe$CCAA)<-list("Andalucía"="01", "Aragón"="02", "Asturias"="03", "Baleares"="04", "Canarias"="05", "Cantabria"="06", "Castilla-León"="07", "Castilla-La Mancha"="08", "Cataluña"="09", "Comunidad Valenciana"="10", "Extremadura"="11", "Galicia"="12", "Madrid"="13", "Murcia"="14", "Navarra"="15", "País Vasco"="16", "Rioja"="17", "Ceuta"="51", "Melilla"="52")
 levels(dframe$PROV)<-lista_provincia
-levels(dframe$SEXO1)<-list("V"="1",  "M"="6")
-levels(dframe$ECIV1)<-list("Soltero"="1", "Casado"="2", "Viudo"="3", "Separado"="4")
-levels(dframe$PRONA1)<-lista_provincia
-levels(dframe$REGNA1)<-lista_region
-levels(dframe$NAC1)<-list("Española"="1",  "Doble"="2", "Extrangera"="3")
-levels(dframe$EXREGNA1)<-lista_region
+levels(dframe$SEXO)<-list("V"="1",  "M"="6")
+levels(dframe$ECIV)<-list("Soltero"="1", "Casado"="2", "Viudo"="3", "Separado"="4")
+levels(dframe$PRONA)<-lista_provincia
+levels(dframe$REGNA)<-lista_region
+levels(dframe$NAC)<-list("Española"="1",  "Doble"="2", "Extrangera"="3")
+levels(dframe$EXREGNA)<-lista_region
 levels(dframe$NFORMA)<-list("AN" = "AN", "P1" = "P1", "P2" = "P2", "S1" = "S1", "SG" = "SG", "SP" = "SP", "SU" = "SU")
 dframe$EDADEST[dframe$EDADEST==0]<-NA # Valor nulo
 levels(dframe$CURSR)<-list("Si" = "1", "En vacaciones" = "2", "No" = "3")
@@ -121,16 +132,41 @@ dframe$SIDI2[dframe$SIDI2=="00"]<-NA # Valor nulo
 dframe$SIDI3[dframe$SIDI3=="00"]<-NA # Valor nulo
 levels(dframe$EMPANT)<-lista_si_no
 levels(dframe$SIDAC1)<-list("Trabajando" = "1", "Buscando Empleo" = "2")
-levels(dframe$SIDAC2)<-list("Trabajando" = "1", "Buscando Empleo" = "2")
-levels(dframe$MUN1)<-list("Mismo" = "1", "Distinto" = "6")
-levels(dframe$PRORE1)<-lista_provincia
-levels(dframe$REPAIRE1)<-lista_region
-levels(dframe$TRAANT)<-lista_si_no
+#levels(dframe$SIDAC2)<-list("Trabajando" = "1", "Buscando Empleo" = "2")
+levels(dframe$MUN)<-list("Mismo" = "1", "Distinto" = "6")
+levels(dframe$PRORE)<-lista_provincia
+levels(dframe$REPAIRE)<-lista_region
+#levels(dframe$TRAANT)<-lista_si_no
 dframe$FACTOREL<-dframe$FACTOREL/100
 
-
-
 #------------- FIN: CADA VEZ
+
+#------------- ARREGLILLOS
+
+dframe$REGNA = ifelse(is.na(dframe$REGNA),dframe$PRONA, dframe$REGNA)
+dframe$PRONA <- NULL
+
+dframe$REGEST = ifelse(is.na(dframe$REGEST),dframe$PROEST, dframe$REGEST)
+dframe$PROEST <- NULL
+
+dframe$REPAIRE = ifelse(is.na(dframe$REPAIRE),dframe$PRORE, dframe$REPAIRE)
+dframe$PRORE <- NULL
+
+dframe$TCONT = ifelse(is.na(dframe$TCONTM),dframe$TCONTD/31, dframe$TCONTM)
+dframe$TCONTM <- NULL
+dframe$TCONTD <- NULL
+
+dframe$SIDAC <- dframe$SIDAC1
+dframe$SIDAC1 <- NULL
+
+
+#----->>>ARREGLAR ESTO
+#dframe$SIDI = paste(ifelse(is.na(dframe$SIDI1),"",dframe$SIDI1),ifelse(is.na(dframe$SIDI2),"",dframe$SIDI2),ifelse(is.na(dframe$SIDI3),"",dframe$SIDI3), sep="")
+#dframe$SIDI = ifelse(dframe$SIDI=="",NA, dframe$SIDI)
+
+
+#------------- FIN: ARREGLILLOS
+
 
 #install.packages("RSQLite")
 library(RSQLite)
