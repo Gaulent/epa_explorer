f37a81d4c207cd29f8ee65ba12c5d70823691255
Jose Sauco <jose.saucod@gmail.com>
2017-02-19 20:56:34 +0100

    Un par de graficas más. Descubrir el criterio para decir que alguien esta en paro

--- test/test_graphs.R
+++ test/test_graphs.R
@@ -7,39 +7,43 @@ params<-list(ciclo=176)
 
 grafica<-0
 
-for (i in 0:6) {
 
-  current_data<-getData("FACTOREL", c("CICLO=",params$ciclo-i*4," AND SITU=7"))
-  past_data<-getData("FACTOREL", c("CICLO=",params$ciclo-i*4-1," AND SITU=7"))
-  grafica[7-i] <- (sum(current_data$FACTOREL) - sum(past_data$FACTOREL))
 
-}
-
-datos_publico<-round(grafica/1000,digits = 1)
 
 for (i in 0:6) {
   
-  current_data<-getData("FACTOREL", c("CICLO=",params$ciclo-i*4," AND SITU<>7"))
-  past_data<-getData("FACTOREL", c("CICLO=",params$ciclo-i*4-1," AND SITU<>7"))
-  grafica[7-i] <- (sum(current_data$FACTOREL) - sum(past_data$FACTOREL))
+  current_data<-getData("FACTOREL", c("CICLO=",params$ciclo-4*i," AND ((BUSCA=1 AND FOBACT=1) OR NUEVEM=1) AND DISP=1"))
+  past_data<-getData("FACTOREL", c("CICLO=",params$ciclo-4*i-1," AND ((BUSCA=1 AND FOBACT=1) OR NUEVEM=1) AND DISP=1"))
+  grafica[7-i] <- sum(current_data$FACTOREL) - sum(past_data$FACTOREL)
   
 }
 
-datos_privado<-round(grafica/1000,digits = 1)
-
-
-
+datos<-round(grafica/1000,digits = 1)
 nombres = c("T3-2010","T3-2011","T3-2012","T3-2013","T3-2014","T3-2015","T3-2016")
-colores = c("red3","gold1")
-main_text = "Evolución del total de ocupados, en tasa anual"
-
-datos <- rbind(datos_publico,datos_privado)
+colores = c("gold1","gold1","gold1","gold1","gold1","gold1","red3")
+main_text = "Evolución intertrimestral de la ocupación, en miles\n(variación del 3er trimestre sobre el 2º del mismo año)"
 
 ylim <- c(1.2*min(datos), 1.2*max(datos))
-bp <- barplot(datos,beside=TRUE,names.arg=nombres, col = colores, border=NA, main=main_text, cex.names=0.75, cex.axis = 0.75,ylim=ylim)
+bp <- barplot(datos,space=1,names.arg=nombres, col = colores, border=NA, main=main_text, cex.names=0.75, cex.axis = 0.75,ylim=ylim)
 text(x = bp, y = datos, label = datos, pos = 3, cex = 0.75,adj = c(0.5,1))
-axis(1,pos=0,labels=FALSE, at = seq(2,20,3),outer=TRUE)
+axis(1,pos=0,labels=FALSE, at = seq(1.5,13.5,2),outer=TRUE)
 abline(h=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
