3748eb2099c27ca53b2d6d8425f72cf66c7137da
Jose Sauco <jose.saucod@gmail.com>
2017-03-02 21:14:10 +0100

    Preparacion para mejora de rendimiento en informe

--- test/test_graphs.R
+++ test/test_graphs.R
@@ -6,6 +6,54 @@ source("../global.R", encoding = "UTF-8")
 
 params<-list(ciclo=176)
 
+
+
+grafica<-0
+
+current_data<-getData("CICLO,SITU,BUSCA,FOBACT,NUEVEM,DISP,CCAA,AOI,FACTOREL", c("CICLO BETWEEN",params$ciclo-25,"AND",params$ciclo))
+
+# Preparar recalculo
+
+
+for (i in 0:6) {
+  
+  current_data<-getData("FACTOREL", c("CICLO=",params$ciclo-4*i," AND SITU IS NOT NULL"))
+  past_data<-getData("FACTOREL", c("CICLO=",params$ciclo-4*i-1," AND SITU IS NOT NULL"))
+  grafica[7-i] <- sum(current_data$FACTOREL) - sum(past_data$FACTOREL)
+  
+}
+
+datos<-round(grafica/1000,digits = 1)
+nombres = c("T3-2010","T3-2011","T3-2012","T3-2013","T3-2014","T3-2015","T3-2016")
+colores = c("gold1","gold1","gold1","gold1","gold1","gold1","red3")
+main_text = "Evolución intertrimestral de la ocupación, en miles\n(variación del 3er trimestre sobre el 2º del mismo año)"
+
+ylim <- c(1.2*min(datos), 1.2*max(datos))
+bp <- barplot(datos,space=1,names.arg=nombres, col = colores, border=NA, main=main_text, cex.names=0.75, cex.axis = 0.75,ylim=ylim)
+text(x = bp, y = datos, label = datos, pos = 3, cex = 0.75,adj = c(0.5,1))
+axis(1,pos=0,labels=FALSE, at = seq(1.5,13.5,2),outer=TRUE)
+abline(h=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 grafica<-0
 
 
