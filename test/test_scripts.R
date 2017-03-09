9c636cd9ba2a79c5236c27839be3cb307702d8f4
Jose Sauco <jose.saucod@gmail.com>
2017-03-09 21:08:46 +0100

    Implementados check y update database

--- test/test_scripts.R
+++ test/test_scripts.R
@@ -2,20 +2,49 @@ library(ggplot2)
 library(RSQLite)
 library(plyr)
 
-source("../global.R", encoding = "UTF-8")
+source("global.R", encoding = "UTF-8")
 
 
 
+# Obtener parches que faltan
 
-library(RCurl)
-filenames <- getURL("ftp://www.ine.es/temas/epa/",ftp.use.epsv = FALSE,dirlistonly = TRUE) 
-filenames<-strsplit(filenames, "\r*\n")
+check_for_updates <- function() {
+  
+  library(RCurl)
+  library(dplyr)
+  ftp_address <- "ftp://www.ine.es/temas/epa/"
+  filenames <- getURL(ftp_address,ftp.use.epsv = FALSE,dirlistonly = TRUE) 
+  filenames<-strsplit(filenames, "\r*\n")
+  
+  df<-data.frame(x=filenames,stringsAsFactors = FALSE)
+  names(df)[1]<-"Name"
+  df<-filter(df,grepl("datos_.+\\.zip", Name))
+  
+  db_updates<-getSQL("SELECT * FROM db_updates")
+  
+  check_for_updates<-setdiff(df,db_updates)
+}
+#--Actualizar base de datos
 
-df<-data.frame(x=filenames,stringsAsFactors = FALSE)
-names(df)[1]<-"x"
-df[grep('datos_.+\\.zip', df$x),]
+update_database <- function(selected_file = "datos_4t16.zip") {
+  
+  tempReport <- file.path(tempdir(), selected_file)
+  #file.copy("reports/report.Rmd", tempReport, overwrite = TRUE)
+  
+  download.file(file.path(ftp_address, selected_file),destfile=tempReport,method="libcurl",quiet=TRUE)
+  df<-unzip(tempReport,list=TRUE)
+  df<-arrange(df, desc(Length) )
+  unzipped_file<-df$Name[1]
+  
+  unzip(tempReport,exdir=tempdir())
+  
+  super_funcion_que_lo_hace_todo_yeah(file.path(ftp_address, unzipped_file))
 
-download.file("ftp://www.ine.es/temas/epa/datos_4t13.zip",destfile="datos_4t13.zip",method="libcurl")
-unzip("datos_4t13.zip")
+  getSQL(paste(c("INSERT INTO db_updates VALUES ('",selected_file,"')"),collapse=""))
+}
+#---------------------------------
+
+
+
 
 
