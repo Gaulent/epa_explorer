96b84e4faf8089d9fa366a2a9b488e802f46210f
Jose Sauco <jose.saucod@gmail.com>
2017-04-11 20:30:12 +0200

    Final del curso de udacity

--- test/test_scripts.R
+++ test/test_scripts.R
@@ -6,32 +6,28 @@ source("libs/update_database.R", encoding = "UTF-8")
 
 library(ggplot2)
 
-#install.packages('ggthemes', dependencies = TRUE) 
-#library(ggthemes) 
-#theme_set(theme_minimal(24)) 
+#Scatterplot
 
-dframe<-getData(c("EDADEST", "DREN"),c("CICLO=176"))
 
-ggplot(aes(x=EDADEST, y=DREN), data = na.omit(dframe)) + 
-  geom_point(alpha=1/20, color = 'orange') + 
-  xlim(7, 60) + 
-  coord_trans(y = 'sqrt') +
-  geom_line(stat='summary', fun.y = mean) + 
-  geom_line(stat='summary', fun.y = quantile, fun.args = list(probs = .1), linetype = 2, color = 'blue') + 
-  geom_line(stat='summary', fun.y = quantile, fun.args = list(probs = .5), color = 'blue') + 
-  geom_line(stat='summary', fun.y = quantile, fun.args = list(probs = .9), linetype = 2, color = 'blue') + 
-  geom_smooth(method = 'lm', color = 'red')
+install.packages("GGally")
+library(GGally)
+#theme_set(theme_minimal(20))
 
-cor.test(dframe$EDADEST, dframe$DREN)
-# 0.3 < significativa, pero pequeña < 0.5 mayor < 0.7
+set.seed(1836)
 
-#xlim y coord_trans hay que quitarlos para poner coord_car
+pf<-getData("*","CICLO=176")
 
-ggplot(aes(x=EDADEST, y=DREN), data = na.omit(dframe)) + 
-  geom_jitter(alpha=1/20) + 
-  xlim(7, 60)
+pf_subset<-pf[,c(4:7)]
+names(pf_subset)
+ggpairs(pf_subset[sample.int(nrow(pf_subset),1000), ])
 
-#meter tambien geom_smooth
+
+ggpairs(pf_subset[sample.int(nrow(pf_subset),1000), ], 
+        lower = list(continuous = wrap("points", shape = I('.'))), 
+        upper = list(combo = wrap("box", outlier.shape = I('.'))))
+
+
+
 
 # -------------------------------- IMPORTANTE -----------------------
 # Para comparar cosas en el tiempo.
