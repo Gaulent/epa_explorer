1e413df281cd7b1f289774b61d20cdc611ced3ab
Jose Sauco <jose.saucod@gmail.com>
2017-05-05 13:45:42 +0200

    Pruebas de clustering y reglas de asociacion

--- test/test_scripts.R
+++ test/test_scripts.R
@@ -6,10 +6,35 @@ source("libs/update_database.R", encoding = "UTF-8")
 
 
 
-library(klaR)
+# Pruebas de Clustering
+# ---------------------------------------------------------
 
 dframe<-getData("*","CICLO=176")
-dframe <- as.data.frame(unclass(dframe))
-kmodes(na.omit(dframe[, 20:24]), 4, iter.max = 2)
 
-# utilizar ??cut para discretizar variables numericas
+dframe <- as.data.frame(unclass(dframe))
+
+indx <- sapply(dframe, is.numeric)
+dframe[indx] <- lapply(dframe[indx], function(x) cut(x,breaks = 5, include.lowest = TRUE, ordered_result = TRUE))
+
+
+dframe <- dframe[sample.int(nrow(dframe),1000), ]
+
+library(klaR)
+kmodes(na.omit(dframe[, 2:5]), 4, iter.max = 2)
+
+# Pruebas de reglas de asociacion
+# ---------------------------------------------------------
+
+dframe<-getData("*","CICLO=176")
+
+dframe <- as.data.frame(unclass(dframe))
+
+indx <- sapply(dframe, is.numeric)
+dframe[indx] <- lapply(dframe[indx], function(x) cut(x,breaks = 5, include.lowest = TRUE, ordered_result = TRUE))
+
+dframe <- dframe[sample.int(nrow(dframe),1000), ]
+
+library(arules)
+# find association rules with default settings
+rules <- apriori(na.omit(dframe[, 2:5]))
+inspect(rules)
